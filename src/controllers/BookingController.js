46449f1ce6540171bd8bacbead58ec3f0a5b921c
Roberto Gomes <robertohgm@hotmail.com>
2020-01-15 23:06:09 -0300

    Ajuste de rota

--- src/controllers/BookingController.js
+++ src/controllers/BookingController.js
@@ -5,7 +5,7 @@ module.exports = {
         const { user_id } = req.headers;
         const { spot_id } = req.params;
         const { date } = req.body;
-
+        
         const booking = await Booking.create({
             user: user_id,
             spot: spot_id,
